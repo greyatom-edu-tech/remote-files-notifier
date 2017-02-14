c56cdf8ac0bce04590269de346efa26a1fb7b60f
surajhell88 <surajhell88@gmail.com>
2017-02-14 15:01:55 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,31 @@
+var io = require('socket.io-client');
+var chokidar = require('chokidar');
+var yaml = require('js-yaml');
+var fs = require('fs');
+var netrc = require('netrc');
+const osHomedir = require('os-homedir');
+
+var connected = false;
+// get workspace path to watch after
+var config = yaml.safeLoad(fs.readFileSync(osHomedir() + '/.ga-config', 'utf8'));
+// get user github username for creating websocket room
+var myNetrc = netrc();
+var room = myNetrc['ga-extra'].login;
+// connect to GreyAtom's websocket server
+var socket = io.connect('http://35.154.96.42:9000', { reconnect: true });
+
+socket.on('connect', function() {
+	connected = true;
+	socket.emit('room', room);
+});
+
+socket.on('message', function(msg){
+	console.log('message', msg);
+});
+
+chokidar.watch(config[":workspace"]).on('all', function(event, path) {
+  	if (connected) {
+  		var typeOfEvent = "remote-file-change";
+		socket.emit('message', '{"room":"'+room+'","type":"'+ typeOfEvent +'","title":"","message":""}');
+  	}
+});
